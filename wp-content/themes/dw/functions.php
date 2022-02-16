4a9483fc375a8bb6f15065a6841dbb669a40a337
thienanvo <thienan_vo@live.be>
2022-02-16 15:02:24 +0100

    the Loop

---
+++ wp-content/themes/dw/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+// Désactiver l'éditeur "Gutenberg" de Wordpress
+add_filter('use_block_editor_for_post', '__return_false');
+
+// Activer les images sur les articles
+add_theme_support('post-thumbnails');
+
+// Enregistrer un seul custom post-type pour "nos voyages"
+register_post_type('trip', [
+    'label' => 'Voyages',
+    'labels' => [
+        'name' => 'Voyages',
+        'singular_name' => 'Voyage',
+    ],
+    'description' => 'Tous les articles qui décrivent un voyage',
+    'public' => true,
+    'menu_position' => 5,
+    'menu_icon' => 'dashicons-palmtree',
+]);
--- wp-content/themes/dw/index.php
+++ wp-content/themes/dw/index.php
@@ -1,13 +1,35 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello</h1>
-</body>
-</html>
+<?php get_header() ?>
+
+<main class="layout">
+    <section class="layout__intro">
+        <h2 class="layout__title">Introduction</h2>
+        <p class="layout__text">Welcome&nbsp;!</p>
+    </section>
+
+    <section class="layout__latest latest">
+        <h2 class="latest__title">Nos dernières nouvelles</h2>
+        <div class="latest__container">
+            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <article class="post">
+                    <a href="<?= get_the_permalink() ?>" class="post__link">Lire l'article "<?= get_the_title() ?>"</a> <!-- position:absolute + z-index -->
+                    <div class="post__card">
+                        <header class="post__head">
+                            <h3 class="post__title"><?= get_the_title() ?></h3>
+                            <p class="post__meta">Publié par <?= get_the_author() ?>, le <time class="post__date" datetime="<?= get_the_date('c') ?>"><?= get_the_date() ?></time></p>
+                        </header>
+                        <figure class="post__fig">
+                            <?= get_the_post_thumbnail(null, 'medium',['class' => 'post__thumb', 'id' => 'test']) ?>
+                        </figure>
+                        <div class="post__excerpt">
+                            <p><?= get_the_excerpt() ?></p>
+                        </div>
+                    </div>
+                </article>
+            <?php endwhile; else : ?>
+                <p>No</p>
+            <?php endif; ?>
+    </div>
+    </section>
+</main>
+
+<?php get_footer() ?>
