626d76c43c6a6633169bf4a3f53cc56bbbc022b8
thienanvo <thienan_vo@live.be>
2022-02-16 15:21:50 +0100

    added trips custom post type

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -6,15 +6,15 @@ add_filter('use_block_editor_for_post', '__return_false');
 // Activer les images sur les articles
 add_theme_support('post-thumbnails');
 
-// Enregistrer un seul custom post-type pour "nos voyages"
-register_post_type('trip', [
-    'label' => 'Voyages',
-    'labels' => [
+// Enregistrer un (type de ressource) seul custom post-type pour "nos voyages"
+register_post_type('trips', [
+    'label' => 'Voyages', // plural
+    'labels' => [ // interface admin
         'name' => 'Voyages',
         'singular_name' => 'Voyage',
     ],
-    'description' => 'Tous les articles qui décrivent un voyage',
-    'public' => true,
+    'description' => 'La ressource permettant de gérer les voyages qui ont été effectués',
+    'public' => true, // accessible dans l'affichage et dans l'interface admin (formulaire de contact : false)
     'menu_position' => 5,
-    'menu_icon' => 'dashicons-palmtree',
+    'menu_icon' => 'dashicons-palmtree', // https://developer.wordpress.org/resource/dashicons/#share-alt
 ]);
