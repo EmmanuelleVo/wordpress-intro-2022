c0570046bb17e88d3dd41b0bbb92e86e06598408
thienanvo <thienan_vo@live.be>
2022-04-20 14:43:26 +0200

    Add js workflow

--- wp-content/themes/dw/resources/js/script.js
+++ wp-content/themes/dw/resources/js/script.js
@@ -1,1 +1,21 @@
-console.log('ok')
+class DW_Controller
+{
+    constructor() {
+        console.log(document.body, 'constructor')
+        // A ce stade-ci, le DOM n'est pas encore prêt car on est dans le <head>
+        // Permet d'instancier les classes utilitaires par exemple
+
+    }
+
+    run() {
+        // Désormais, le DOM est prêt, on peut commencer à manipuler
+        // Permet d'instancier les classes de composants d'interface par exemple
+        console.log(document.body, 'OK')
+        // ici : this.responsiveMenu = new ResponsiveMenu()
+    }
+
+}
+window.dw = new DW_Controller()
+window.addEventListener('load', () => {
+    window.dw.run()
+})
